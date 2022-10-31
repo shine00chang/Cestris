557d753b31023dfbc797255f103e5b61a116186f
Shine Chang <shine00chang@gmail.com>
2022-10-30 17:40:46 -0700

    UI overhaul, added transitions to menu.

--- public/index.js
+++ public/index.js
@@ -3,25 +3,78 @@ import OnlineDriver from './onlineDriver.mjs';
 import Chat from './chat.mjs';
 import { io } from "https://cdn.socket.io/4.4.1/socket.io.esm.min.js";
 
+// Trigger load animations
+slideMenuIn(document.getElementById('home-menu'));
+
+
 let socket = io('localhost:8000');
-let roomId;
 let game;
 let chat = new Chat(socket, document.getElementById('chat-box'));
-
 chat.startListeners();
 
+
+function titleToConerTransition() {
+    document.getElementById('title-box').style.top = '30px';
+    document.getElementById('title-box').style.left = '70px';
+}
+
+
+function slideMenuIn (elem) {
+    elem.style.top = '40%';
+    elem.style.opacity = 1;
+}
+
+
+function slideMenuOut (elem) {
+    elem.style.top = '100%';
+    elem.style.opacity = 0;
+}
+
+
+function  activatePrompt (elem) {
+    elem.style.display = 'inline';
+    document.getElementById('filter').style.opacity = 0.5;
+}
+
+
+function startGameView () {
+    titleToConerTransition();
+    document.getElementById('main-box').style.display = 'flex';
+    document.getElementById('main-box').style.top = '10%';
+}
+
+
+function decativatePrompt (elem) {
+    elem.style.display = 'none';
+    document.getElementById('filter').style.opacity = 0;
+}
+
+
 document.getElementById('local-button').onclick = () => {
+    startGameView();
+    slideMenuOut(document.getElementById('home-menu'));
+
     if (game !== undefined) game.destruct();
 
     game = new LocalDriver(document.getElementById('main-view'));
     setTimeout(game.start, 3000);
 }
 
+
 document.getElementById('online-join-button').onclick = onOnlineJoin;
+document.getElementById('online-prompt-cancel').onclick = () => decativatePrompt(document.getElementById('online-prompt'));
+document.getElementById('online-prompt-submit').onclick = onlinePromptSubmit;
+
 
 function onOnlineJoin () {
     if (game !== undefined) return;
 
+    // Activate user & room ID prompt.
+    activatePrompt(document.getElementById('online-prompt'));
+}
+
+
+function onlinePromptSubmit () {
     // Send 'online-join' event to signal join
     const roomIdIn = document.getElementById('roomid-input').value;
     const nameIn = document.getElementById('name-input').value;
@@ -70,7 +123,15 @@ function onOnlineJoin () {
     // switch callback function to online-leave
     document.getElementById('online-join-button').innerText = 'online-leave';
     document.getElementById('online-join-button').onclick = onOnlineLeave;
+
+    // switch menus (trigger transitions)
+    slideMenuOut(document.getElementById('home-menu'));
+    slideMenuIn(document.getElementById('online-menu'));
+    
+    // deactivate prompt
+    decativatePrompt(document.getElementById('online-prompt'));
 }
+
 
 function onOnlineLeave () {
     socket.emit('online-leave');
@@ -82,12 +143,17 @@ function onOnlineLeave () {
     // switch callback function to online-join
     document.getElementById('online-join-button').innerText = 'online-join';
     document.getElementById('online-join-button').onclick = onOnlineJoin;
+
+    // switch menus (trigger transitions)
+    slideMenuIn(document.getElementById('home-menu'));
+    slideMenuOut(document.getElementById('online-menu'));
 }
-
-
 
 function onOnlineReady () {
     if (game !== undefined) return;
+    startGameView();
+    slideMenuOut(document.getElementById('online-menu'));
+
 
     socket.emit('online-ready');
 
